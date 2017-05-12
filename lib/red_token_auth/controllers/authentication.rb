aba72acd65cc218e4de655934afa4ead08e5a6ed
caio ergos <caioergos@gmail.com>
2017-05-12 20:26:53 -0300

    WIP: controller methods

--- lib/red_token_auth/controllers/authentication.rb
+++ lib/red_token_auth/controllers/authentication.rb
@@ -3,24 +3,34 @@ module Controllers
     extend ActiveSupport::Concern
 
     class_methods do
-      def authenticate(klass, options = {})
-        klass = klass.to_s.capitalize.constatize
-        auth_key = options[:auth_key] || :email
+      # class UserController < ApplicationController
+      #   before_action only: [:index] { authenticate :user }
+      # end
+      def authenticate(resource, options = {})
+        klass = resource.to_s.capitalize.constatize
+
+        auth_key   = options[:auth_key]   || :email
         auth_param = options[:auth_param] || :email
 
-        @resource = klass.find_by(auth_key: headers[:"#{auth_param}"])
+        resource = klass.find_by(auth_key: headers[:"#{auth_param}"])
 
-        if entity.authenticate_token(headers[:"#{klass.to_s.downcase}-#{auth_param}"])
-          @resource
-        else
+        unless entity.authenticate_token(headers[:"#{klass.to_s.downcase}-#{auth_param}"])
           render_unauthorized
         end
       end
     end
 
     included do
-      def render_unauthorized(errors)
-        render head: :ok, status: :unauthorized
+      def resource_name(klass)
+        klass.to_s.downcase.to_sym
+      end
+    end
+
+    private
+
+    def generate_methods
+      define_method(:"current_#{resource_name(klass)}") do
+        resource
       end
     end
   end
